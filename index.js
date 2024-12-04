cce13bf4497c3018ace40a9083060eab20d01710
Antonio Luis García Laredo <104435989+anluisgl@users.noreply.github.com>
2024-12-04 11:54:17 +0100

    ddd

--- index.js
+++ index.js
@@ -5,31 +5,38 @@
  * Repository: https://github.com/anluisgl
  */
 
-const lightRedSpan = document.querySelector('.light.red');
+const lightRedSpan = document.querySelector('.light.red')
 
 lightRedSpan.addEventListener('click', function() {
 
   document.getElementById('box').style.display = 'none';
-});
+})
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const aboutMeImage = document.querySelector('.imgfolder[src="img/inconosmac1.png"]');
-  aboutMeImage.addEventListener('click', () => updateFolderContent(`
+  const aboutMeImg = document.querySelector('.imgfolder[src="img/inconosmac1.png"]');
+  aboutMeImg.addEventListener('click', () => updateFolderContent(`
       <div class="aboutmejs">
           <h1>TEST ABOUT ME</h1>
       </div>
-  `));
+  `))
 
-  const knowledgeImage = document.querySelector('.imgfolder[src="img/folderknow.png"]');
-  knowledgeImage.addEventListener('click', () => updateFolderContent(`
-      <div class="conocimientosjs">
+  const knowledgeImg = document.querySelector('.imgfolder[src="img/folderknow.png"]');
+  knowledgeImg.addEventListener('click', () => updateFolderContent(`
+      <div class="knowledgejs">
           <h1>TEST CONOCIMIENTOS</h1>
       </div>
-  `));
-});
+  `))
+
+  const ProjectImg = document.querySelector('.imgfolder[src="img/folderwork.png"]')
+  ProjectImg.addEventListener('click', () => updateFolderContent(`
+      <div class="proyectsjs">
+          <h1>TEST PROJECTS</h1>
+      </div>
+  `))
+})
 
 function updateFolderContent(content) {
-  const folder2 = document.getElementById("folder2");
-  folder2.innerHTML = content;
+  const folder2 = document.getElementById("folder2")
+  folder2.innerHTML = content
 }
