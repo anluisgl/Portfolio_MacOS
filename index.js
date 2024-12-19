bf117fa1a5360112e9354ec9b2b5cac202e650bc
Antonio Luis García Laredo <104435989+anluisgl@users.noreply.github.com>
2024-12-19 12:11:47 +0100

    xjxñ

--- index.js
+++ index.js
@@ -18,3 +18,26 @@ function clickButton(e){
 links.forEach(link => {
   link.addEventListener("click", clickButton)
 })
+
+
+
+
+
+
+
+
+/* https://configuroweb.com/construye-tu-propio-reloj-digital-en-javascript-guia-paso-a-paso/#google_vignette */
+function actualizarHora() {
+  const reloj = document.getElementById('reloj');
+  const ahora = new Date();
+  const horas = agregarCero(ahora.getHours());
+  const minutos = agregarCero(ahora.getMinutes());
+  reloj.textContent = `${horas}:${minutos}`;
+}
+
+function agregarCero(numero) {
+  return numero < 10 ? '0' + numero : numero;
+}
+
+setInterval(actualizarHora, 1000);
+actualizarHora();
